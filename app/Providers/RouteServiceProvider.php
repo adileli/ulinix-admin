8d24ae02ec583016e44cf02768ef716f3422cbe2
adil <wow3598@163.com>
2020-04-09 17:58:05 +0800

    重复访问login会跳转到后台首页

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,6 +23,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/';
 
+    public const  ADMIN_HOME = '/admin';
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
